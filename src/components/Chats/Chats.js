6bbff67bdd7bfaeeb17b512059848c805cab5626
Nina <frontend-developer@inbox.ru>
2021-11-25 13:22:34 +0300

    На страницу Chats добавила вывод информации, какой чат выбран

--- src/components/Chats/Chats.js
+++ src/components/Chats/Chats.js
@@ -3,13 +3,18 @@ import { Col, Container, Row } from "react-bootstrap";
 import ChatList from "../ChatList/ChatList";
 import MessagesList from "../MessagesList/MessagesList";
 import FormMess from "../Form/Form";
-import { Navigate,  useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectMessages } from "../../store/messages/selectors";
+import { selectChatsList, } from "../../store/chats/selectors";
 
 function Chats() {
   const { chatId } = useParams();
   const messages = useSelector( selectMessages );
+  const chats = useSelector( selectChatsList );
+  const chatName = (chats) => {
+    return chats.find( chat => chat.id === chatId ).name;
+  };
 
   if( !messages[ chatId ] ) {
     return <Navigate replace to="/chats"/>;
@@ -22,6 +27,7 @@ function Chats() {
           <ChatList/>
         </Col>
         <Col sm={ 11 } md={ 6 }>
+          <h2 className="mb-3">Вы находитесь на странице чата { chatName( chats ) }</h2>
           <MessagesList messages={ messages[ chatId ] }/>
           <FormMess/>
         </Col>

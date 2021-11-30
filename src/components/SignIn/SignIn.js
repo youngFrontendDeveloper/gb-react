34321ba252196da8f9d92d17da6cde00e43c41df
Nina <frontend-developer@inbox.ru>
2021-11-30 18:15:42 +0300

    Пыталась исправить ошибку перезагрузки страницы при LogIn

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,18 +1,22 @@
 import { useState } from "react";
 import ModalSign from "../ModalSign/ModalSign";
 import { logIn } from "../../services/firebase";
+import { signIn } from "../../store/profile/actions";
 import "./SignIn.css";
+import { useDispatch } from "react-redux";
 
 function SignIn() {
   const [modalShow, setModalShow] = useState(false);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
 
   const handleSingIn = async (email, pass) => {
     setError("");
     setLoading(true);
     try {
       await logIn(email, pass);
+      dispatch(signIn);
       setModalShow(false);
     } catch (err) {
       console.log(err);
